57a894f53b41e980745a349fb9f7256de23c3ba5
crisbeto <crisbeto@abv.bg>
2016-12-25 17:32:39 +0200

    Stub out the MutationObserver.

--- src/lib/core/observe-content/observe-content.spec.ts
+++ src/lib/core/observe-content/observe-content.spec.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {async, TestBed} from '@angular/core/testing';
+import {async, TestBed, ComponentFixture, fakeAsync, tick} from '@angular/core/testing';
 import {ObserveContentModule} from './observe-content';
 
 /**
@@ -8,17 +8,17 @@ import {ObserveContentModule} from './observe-content';
  */
 
 describe('Observe content', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ObserveContentModule],
-      declarations: [ComponentWithTextContent, ComponentWithChildTextContent],
-    });
+  describe('basic usage', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [ObserveContentModule],
+        declarations: [ComponentWithTextContent, ComponentWithChildTextContent]
+      });
 
-    TestBed.compileComponents();
-  }));
+      TestBed.compileComponents();
+    }));
 
-  describe('text content change', () => {
-    it('should call the registered for changes function', done => {
+    it('should trigger the callback when the content of the element changes', done => {
       let fixture = TestBed.createComponent(ComponentWithTextContent);
       fixture.detectChanges();
 
@@ -33,10 +33,8 @@ describe('Observe content', () => {
       fixture.componentInstance.text = 'text';
       fixture.detectChanges();
     });
-  });
 
-  describe('child text content change', () => {
-    it('should call the registered for changes function', done => {
+    it('should trigger the callback when the content of the children changes', done => {
       let fixture = TestBed.createComponent(ComponentWithChildTextContent);
       fixture.detectChanges();
 
@@ -52,6 +50,55 @@ describe('Observe content', () => {
       fixture.detectChanges();
     });
   });
+
+  describe('debounced', () => {
+    let fixture: ComponentFixture<ComponentWithDebouncedListener>;
+    let callbacks: Function[];
+    let invokeCallbacks = (args?: any) => callbacks.forEach(callback => callback(args));
+
+    beforeEach(async(() => {
+      callbacks = [];
+
+      TestBed.configureTestingModule({
+        imports: [ObserveContentModule],
+        declarations: [ComponentWithDebouncedListener],
+        providers: [{
+          provide: MutationObserver,
+          useValue: function(callback: Function) {
+            callbacks.push(callback);
+
+            return {
+              observe: () => {},
+              disconnect: () => {}
+            };
+          }
+        }]
+      });
+
+      TestBed.compileComponents();
+
+      fixture = TestBed.createComponent(ComponentWithDebouncedListener);
+      fixture.detectChanges();
+    }));
+
+    it('should debounce the content changes', fakeAsync(() => {
+      invokeCallbacks();
+      invokeCallbacks();
+      invokeCallbacks();
+
+      tick(500);
+      expect(fixture.componentInstance.spy).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should should keep track of and merge all of the mutation records', fakeAsync(() => {
+      invokeCallbacks([1]);
+      invokeCallbacks([2]);
+      invokeCallbacks([3]);
+
+      tick(500);
+      expect(fixture.componentInstance.spy).toHaveBeenCalledWith([1, 2, 3]);
+    }));
+  });
 });
 
 
@@ -65,4 +112,12 @@ class ComponentWithTextContent {
 class ComponentWithChildTextContent {
   text = '';
   doSomething() {}
+}
+
+@Component({
+  template: `<div (cdkObserveContent)="spy($event)" [debounce]="debounce">{{text}}</div>`
+})
+class ComponentWithDebouncedListener {
+  debounce = 500;
+  spy = jasmine.createSpy('MutationObserver callback');
 }
--- src/lib/core/observe-content/observe-content.ts
+++ src/lib/core/observe-content/observe-content.ts
@@ -7,7 +7,8 @@ import {
   Input,
   EventEmitter,
   OnDestroy,
-  AfterContentInit
+  AfterContentInit,
+  Injector,
 } from '@angular/core';
 
 import {debounce} from '../util/debounce';
@@ -31,7 +32,7 @@ export class ObserveContent implements AfterContentInit, OnDestroy {
   /** Debounce interval for emitting the changes. */
   @Input() debounce: number;
 
-  constructor(private _elementRef: ElementRef) {}
+  constructor(private _elementRef: ElementRef, private _injector: Injector) { }
 
   ngAfterContentInit() {
     let callback: MutationCallback;
@@ -51,7 +52,7 @@ export class ObserveContent implements AfterContentInit, OnDestroy {
       callback = (mutations: MutationRecord[]) => this.event.emit(mutations);
     }
 
-    this._observer = new MutationObserver(callback);
+    this._observer = new (this._injector.get(MutationObserver))(callback);
 
     this._observer.observe(this._elementRef.nativeElement, {
       characterData: true,
@@ -70,7 +71,11 @@ export class ObserveContent implements AfterContentInit, OnDestroy {
 
 @NgModule({
   exports: [ObserveContent],
-  declarations: [ObserveContent]
+  declarations: [ObserveContent],
+  providers: [
+    // Pass the MutationObserver through DI so it can be stubbed when testing.
+    { provide: MutationObserver, useValue: MutationObserver }
+  ]
 })
 export class ObserveContentModule {
   static forRoot(): ModuleWithProviders {
